e1b833ce0a560968ed1ca2ec20f5c12184fec35e
hamlett-snapit <hamlett.snapit@gmail.com>
2020-05-04 15:17:09 -0500

    Chapter 1 notes

---
+++ week 2/chapter1notes.js
@@ -0,0 +1,21 @@
+var winners = 2;
+// create a variable with the var keyword:  var variableName = somevalue;
+var name = "Duke";
+
+var isEligible = false; 
+
+var nothingHere;
+
+var allGone;
+
+var _thisIsGood;
+
+var $soIsThis;
+
+// These Are not 
+// break
+// continue
+// interface
+// return
+// public
+
