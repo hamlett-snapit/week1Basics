7e0e066a7de1d8e246d6512d1f1c8ab9baf81696
hamlett-snapit <hamlett.snapit@gmail.com>
2020-05-05 09:55:26 -0500

    added a page

---
+++ week 2/chp1hmwk1.js
@@ -0,0 +1,9 @@
+// Create 10 variables. All have to have different values. 
+// Once you have created the variables, create one last 
+// variable with all the variables added together.
+// HINT: use the + sign to add variables together. 
+
+// DO NOT DO THIS ??
+// var a = 1
+// var b = 1
+// var c = 1
