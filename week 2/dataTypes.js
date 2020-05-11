108963ed2c3cb792f37358d99836a396dda69e4e
hamlett-snapit <hamlett.snapit@gmail.com>
2020-05-11 11:08:58 -0500

    new week new code

---
+++ week 2/dataTypes.js
@@ -0,0 +1,92 @@
+// Numbers
+var numx = 5; 
+var y = 8;
+var z = 12;
+console.log(numx);
+
+// Strings 
+
+var strx = "Ramone H";
+var y = " Family of 6";
+var z = " Love My Wife";
+console.log(strx);
+
+// Decimals
+
+var x = 10.4;
+var y = 22.8;
+var z = 0.6;
+
+// Scientific Notation
+
+var x = 345e5;
+var y = 123e7;
+var z = 503e8;
+
+// Boolean 
+
+var x = true;
+var y = false;
+
+// Arrays 
+
+var x = [1,2,3,4]
+var y = ["hello", "world"]
+
+// Object 
+
+var house = {
+    doors : "four",
+    windows : "ten",
+    garages : "three"
+}
+
+var car = {
+    doors : 2,
+    audio : true,
+    color : "steelblue"
+}
+
+// Undefined Objects 
+
+var x;
+var y;
+var z; 
+
+// Empty Object
+
+var x = null;
+var y = null;
+var z = null;
+
+// Type Of Operator 
+
+// Finds the type of a variable
+// Type String 
+
+
+var str = " Lets PLay a couple games of PING Pong " 
+// display variables on html 
+document.getElementById("wOD").innerHTML = str;
+
+
+var result = typeof(str); 
+
+// Type Number 
+var num = 94843;
+var result2 = typeof(num);
+
+// Type Boolean 
+var boo = true; 
+var result3 = typeof(boo);
+
+// Type Null 
+var empty = null;
+var result4 = typeof(empty);
+
+// Type Undefined
+var undefy;
+var result5 = typeof(undefy);
+
+
+
---
+++ week 3/variables.js
@@ -0,0 +1,28 @@
+// First we need the Var keyword
+var thisVariable;
+document.getElementById("demo").innerHTML = thisVariable; // This is the first link 
+var anotherOne;
+document.getElementById("ramone")
+var thelLastTrain;
+// Next we need a name 
+var myVariable ; // Declare a Variable
+// Next we need to add a Data Type 
+myVariable = 100 ; // Instantiate Value , set a value to myVariable 
+// Change the value of myVariable . 
+myVariable = " One Hundred ";
+thisVariable = "money";
+document.getElementById("demo2").innerHTML = thisVariable; 
+thisVariable = 3.33 ;
+document.getElementById("demo3").innerHTML = thisVariable; 
+thisVariable = 45.64; 
+document.getElementById("demo4").innerHTML = thisVariable; 
+myVariable = 11e3;
+anotherOne = true
+thelLastTrain = [1,2,3,4,5,6];
+thelLastTrain = ["one", "two", "three"];
+anotherOne = {
+    name: "ramone",
+    favColor: "blue"
+}
+thisVariable = undefined;
+document.getElementById("demo5").innerHTML = thisVariable; 
