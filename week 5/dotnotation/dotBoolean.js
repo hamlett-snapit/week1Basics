b459aad355284c9a07a61366fa9d92d66fe83308
hamlett-snapit <hamlett.snapit@gmail.com>
2020-05-27 11:21:32 -0500

    dates & bools

---
+++ week 5/dotnotation/dotBoolean.js
@@ -0,0 +1,12 @@
+var mybool = true;
+var result ;
+// 
+result = mybool.toString();
+console.log(mybool);
+console.log(result);
+
+result = mybool.valueOf();
+console.log(result);
+
+result = mybool.toString().toUpperCase().repeat(5);
+console.log(result);
